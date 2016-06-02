073eba06e5f8febb4fd5d230d3ee8e1612485cf0
ZiaxDK <ziaxdk@gmail.com>
2016-06-02 10:53:49 +0200

    added basic auth

--- server.ts
+++ server.ts
@@ -1,9 +1,13 @@
 import * as Express from 'express';
 import * as Multer from 'multer';
+import * as BasicAuth from 'basic-auth';
 
 let uploadPath = process.env.uploadPath || './uploads';
 let port = process.env.port || 3000;
+let username_ = process.env.user || 'user';
+let password_ = process.env.pass || 'pass';
 
+/* Multer */
 let storage = Multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, uploadPath)
@@ -15,13 +19,35 @@ let storage = Multer.diskStorage({
 
 let upload = Multer({ storage: storage })
 
+/* BasicAuth */
+var auth = function(req, res, next) {
+  function unauthorized(res) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.sendStatus(401);
+  };
+
+  var user = BasicAuth(req);
+
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  };
+
+  if (user.name === username_ && user.pass === password_) {
+    return next();
+  } else {
+    return unauthorized(res);
+  };
+};
+
+/* Express */
 let app = Express();
 
-app.post('/upload', upload.single('ziax'), function(req, res, next) {
+app.post('/upload', auth, upload.single('ziax'), function(req, res, next) {
   res.sendStatus(200);
 });
 
+
 app.listen(port, function() {
-  console.log('Server running on port %s and will use %s as folder', port, uploadPath);
+  console.log('Server running on port %s and will use "%s" as folder and "%s" as user', port, uploadPath, username_);
 });
 
